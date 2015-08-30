32a8dc05a5163a90f8814891f4c32f4f1ca0568b
松下慶大 <atiek1121@gmail.com>
2015-08-30 21:42:29 +0900

    Parse導入 - 必要なフレームワークの追加 - AppdelegateにParseで取得した[Application ID]と[Client Key]を指定

--- Pictweet-iOS/Pictweet-iOS/AppDelegate.swift
+++ Pictweet-iOS/Pictweet-iOS/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("d894vwrbQ7ppC9AGB405TWzB7xmQuCHc4K0ZlclJ", clientKey: "ZopS9inVtva5msQwiL10eapSwhUaltybVK1ZvrKt")
         return true
     }
 

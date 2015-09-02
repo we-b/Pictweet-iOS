fe4eb0c8171be3ee514995e0fcd11c965015232f
松下慶大 <atiek1121@gmail.com>
2015-09-02 19:50:29 +0900

    ツイート投稿画面のUI作成

--- Pictweet-iOS/Pictweet-iOS/NewTweetViewController.swift
+++ Pictweet-iOS/Pictweet-iOS/NewTweetViewController.swift
@@ -2,18 +2,31 @@
 //  NewTweetViewController.swift
 //  Pictweet-iOS
 //
-//  Created by 松下慶大 on 2015/08/30.
+//  Created by 松下慶大 on 2015/08/31.
 //  Copyright (c) 2015年 matsushita keita. All rights reserved.
 //
 
 import UIKit
 
 class NewTweetViewController: UIViewController {
+    @IBOutlet weak var tweetImageView: UIImageView!
+    @IBOutlet weak var imageSelectButton: UIButton!
+    @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileIconLabel: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageSelectButton.layer.cornerRadius = 5
+        profileIconLabel.makeCircle()
+        
+        let gesture = UITapGestureRecognizer(target: self, action: "tapView:")
+        view.addGestureRecognizer(gesture)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "＜", style: UIBarButtonItemStyle.Plain, target: self, action: "backToTweetsViewController")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +34,18 @@ class NewTweetViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func backToTweetsViewController() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func tapImageSelectButton(sender: UIButton) {
+        //カメラロールアクセスを記述
+    }
+    
+    func tapView(sender: UITapGestureRecognizer) {
+        tweetTextView.resignFirstResponder()
+    }
 
     /*
     // MARK: - Navigation
@@ -33,3 +58,13 @@ class NewTweetViewController: UIViewController {
     */
 
 }
+
+
+
+
+
+
+
+
+
+
